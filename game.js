14af7a6b2e3fe717797bab1dff2c05db5204fbfa
Alicodedev <paradox2840@gmail.com>
2022-08-27 21:48:19 -0400

    player & Computer score counter complete

--- game.js
+++ game.js
@@ -5,14 +5,26 @@ function getComputerChoice(x){
 	return x;
 }
 
+var playerscore = 0;
+var compscore = 0;
 function game(){ 
 	const rounds = prompt("Enter amount of rounds you'd like to play: ");
-  var playerscore = 0;
-  var compscore = 0;
-  const playerSelection = prompt("Player turn");
-	const computerSelection = prompt("Computer turn");
+  
 	for(var i = 0; i < rounds; ++i){ // rounds that game will be ran
+    const playerSelection = prompt("Player turn");
+		const computerSelection = prompt("Computer turn");
 		playround(playerSelection,computerSelection);
+  
+    if (i+1 == rounds){
+    	if (playerscore > compscore){
+      	console.log(`Player wins by ${playerscore} points`);
+      }
+    	
+      if (compscore > playerscore){
+      	console.log(`Computer wins by ${compscore} points`);
+      }
+    }
+    
 	}
 }
 
@@ -22,31 +34,37 @@ function playround(playerSelection,computerSelection){
 	while (i == true){
   if (playerSelection == "rock" && computerSelection == "paper"){
   	console.log("player wins");
+    playerscore +=1;// playerscore counter
     i = false;//Break 
   	}
     
   if (playerSelection == "paper" && computerSelection == "rock"){
   	console.log("Computer wins");
+    compscore +=1;
     i = false;
   	}
     
   if (playerSelection == "rock" && computerSelection == "sisscors"){
   	console.log("player wins");
+    playerscore +=1;
     i = false;
   	}
     
   if (playerSelection == "sisscors" && computerSelection == "rock"){
   	console.log("Computer wins");
+    compscore +=1;//computer score counter
     i = false;
   	}
     
   if (playerSelection == "sisscors" && computerSelection == "paper"){
   	console.log("player wins");
+    playerscore +=1;
     i = false;
   	}
     
   if (playerSelection == "paper" && computerSelection == "sisscors"){
   	console.log("Computer wins");
+    compscore +=1;
     i = false; 
   	}
     
