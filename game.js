913ae8e94898d9c7408c3ecaf3474495d3409072
Alicodedev <paradox2840@gmail.com>
2022-08-28 23:49:14 -0400

    Complete computer autoselection & any variation of user input

--- game.js
+++ game.js
@@ -1,18 +1,18 @@
 var arr = ["rock", "paper","scissors"];
+var playerscore = 0;
+var compscore = 0;
 
 function getComputerChoice(x){
 	var x = x[Math.floor(Math.random() * x.length)];
 	return x;
 }
 
-var playerscore = 0;
-var compscore = 0;
 function game(){ 
 	const rounds = prompt("Enter amount of rounds you'd like to play: ");
   
 	for(var i = 0; i < rounds; ++i){ // rounds that game will be ran
-    const playerSelection = prompt("Player turn");
-		const computerSelection = prompt("Computer turn");
+    var playerSelection = prompt("Player turn").toLowerCase();
+		var computerSelection = getComputerChoice(arr).toLowerCase();
 		playround(playerSelection,computerSelection);
   
     if (i+1 == rounds){
@@ -33,36 +33,36 @@ function playround(playerSelection,computerSelection){
 	var i = true; // flag 
 	while (i == true){
   if (playerSelection == "rock" && computerSelection == "paper"){
-  	console.log("player wins");
-    playerscore +=1;// playerscore counter
+  	console.log("Computer wins");
+    compscore +=1;// playerscore counter
     i = false;//Break 
   	}
     
   if (playerSelection == "paper" && computerSelection == "rock"){
-  	console.log("Computer wins");
-    compscore +=1;
+  	console.log("Player wins");
+    playerscore +=1;
     i = false;
   	}
     
-  if (playerSelection == "rock" && computerSelection == "sisscors"){
+  if (playerSelection == "rock" && computerSelection == "scissors"){
   	console.log("player wins");
     playerscore +=1;
     i = false;
   	}
     
-  if (playerSelection == "sisscors" && computerSelection == "rock"){
+  if (playerSelection == "scissors" && computerSelection == "rock"){
   	console.log("Computer wins");
     compscore +=1;//computer score counter
     i = false;
   	}
     
-  if (playerSelection == "sisscors" && computerSelection == "paper"){
+  if (playerSelection == "scissors" && computerSelection == "paper"){
   	console.log("player wins");
     playerscore +=1;
     i = false;
   	}
     
-  if (playerSelection == "paper" && computerSelection == "sisscors"){
+  if (playerSelection == "paper" && computerSelection == "scissors"){
   	console.log("Computer wins");
     compscore +=1;
     i = false; 
