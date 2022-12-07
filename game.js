587d3706c3e5c183f3d9ed842f90ae506cbc6b6c
Alicodedev <paradox2840@gmail.com>
2022-12-06 21:13:44 -0500

    Both player begin with same image file by default

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ const btn_scissors = document.querySelector('.scissors');
 let image = document.querySelector('#user_choice > img'); // accessing img element within div of that id
 let image_bot = document.querySelector('#bot_choice > img')
 
+
 function getComputerChoice(x){ // generates random choices for computer
 	var x = x[Math.floor(Math.random() * x.length)];
 	return x;
@@ -15,25 +16,33 @@ function getComputerChoice(x){ // generates random choices for computer
 
 // playser event choices selection
 btn_rock.addEventListener('click', () =>{
-    
+    let computerSelection = getComputerChoice(arr).toLowerCase();
     image.src = "images/rock.png";
+    image_bot.src = `images/${computerSelection}.png`;
     let playerSelection = arr[0].toLowerCase();
-    game(playerSelection);
+    image_bot.src = `images/${computerSelection}.png`;
+    game(playerSelection,computerSelection);
   
   });
   
 btn_paper.addEventListener('click', () =>{
+    let computerSelection = getComputerChoice(arr).toLowerCase();
     image.src = "images/paper.png";
+    image_bot.src = `images/${computerSelection}.png`;
     let playerSelection = arr[1].toLowerCase();
-    game(playerSelection);
+    
+    game(playerSelection,computerSelection);
   
   });
   
 btn_scissors.addEventListener('click', () =>{
+    let computerSelection = getComputerChoice(arr).toLowerCase();// runs computer choice in parallel with player event
     image.src = "images/scissors.png";
+    image_bot.src = `images/${computerSelection}.png`;
     let playerSelection = arr[2].toLowerCase();// player variable == button type 
     // call game function 
-    game(playerSelection);
+    
+    game(playerSelection,computerSelection);
   
   });
   
@@ -42,13 +51,12 @@ btn_scissors.addEventListener('click', () =>{
 //var rounds = prompt("Enter amount of rounds you'd like to play: ");
 
 
-function game(user_in){ 
+function game(user_in,bot_in){ 
   
 	for(let i = 0; i < 1; ++i){ // rounds that game will be ran
-    
-		let computerSelection = getComputerChoice(arr).toLowerCase();
-    image_bot.src = `images/${computerSelection}.png`; // bot selection is displayed
-		playround(user_in,computerSelection);
+
+		
+		playround(user_in,bot_in);
   
     if (i+1 == 1){
     	if (playerscore <= 5){
@@ -83,45 +91,45 @@ function game(user_in){
 }
 
 //gamerounds function
-function playround(user_in,computerSelection){
+function playround(user_in,bot_in){
 	let i = true; // flag 
-  if (user_in == "rock" && computerSelection == "paper"){
+  if (user_in == "rock" && bot_in == "paper"){
   	console.log("Computer wins");
     compscore +=1;// playerscore counter
     i = false;//Break 
   	}
     
-  if (user_in == "paper" && computerSelection == "rock"){
+  if (user_in == "paper" && bot_in == "rock"){
   	console.log("Player wins");
     playerscore +=1;
     i = false;
   	}
     
-  if (user_in == "rock" && computerSelection == "scissors"){
+  if (user_in == "rock" && bot_in == "scissors"){
   	console.log("player wins");
     playerscore +=1;
     i = false;
   	}
     
-  if (user_in == "scissors" && computerSelection == "rock"){
+  if (user_in == "scissors" && bot_in == "rock"){
   	console.log("Computer wins");
     compscore +=1;//computer score counter
     i = false;
   	}
     
-  if (user_in == "scissors" && computerSelection == "paper"){
+  if (user_in == "scissors" && bot_in == "paper"){
   	console.log("player wins");
     playerscore +=1;
     i = false;
   	}
     
-  if (user_in == "paper" && computerSelection == "scissors"){
+  if (user_in == "paper" && bot_in == "scissors"){
   	console.log("Computer wins");
     compscore +=1;
     i = false; 
   	}
     
-	else if(user_in == computerSelection){
+	else if(user_in == bot_in){
     console.log("tie");
     const element = document.getElementById("winner");
     element.textContent = 'Tie';
